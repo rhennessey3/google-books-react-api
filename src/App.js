c43494b969f908bdd58776d8809fa3a4facda852
Richard Hennessey III <Rick@Richards-iMac.local>
2020-06-12 18:00:09 -0400

    filters working

--- src/App.js
+++ src/App.js
@@ -8,23 +8,32 @@ class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { query: '' }
-    this.handlequery = this.handlequery.bind(this)
+    this.state = { query: '', filterType: '', printType: '', books: [] }
+    // this.handlequery = this.handlequery.bind(this)
   }
 
-  handlequery(event) {
-    this.setState({ query: event.target.value })
-  }
+  handleChange = (event) => {
+    console.log(event.target.id, event.target.value)
+    this.setState({ [event.target.id]: event.target.value })
 
+  }
 
   // https://cors-anywhere.herokuapp.com/
   // This is where we can do our api call - 
   handleSubmit = (event) => {
-    event.preventDefault();
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.query}&key=AIzaSyDSmvyCyGRyNFq0hkSaU1MeqHnmDQ867-I`
+    if (event) {
+      event.preventDefault();
+    }
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${this.state.query}&key=AIzaSyDSmvyCyGRyNFq0hkSaU1MeqHnmDQ867-I`
+    if (this.state.filterType) {
+      url += `&filter=${this.state.filterType}`
+    }
+    if (this.state.printType) {
+      url += `&printType=${this.state.printType}`
+    }
+
     const options = {
       method: 'GET',
-
     }
 
     fetch(url, options)
@@ -37,6 +46,7 @@ class App extends Component {
       .then(res => res.json())
       .then(data => {
         this.setState({
+          books: data.items || [],
           title: "",
           author: "",
           listPrice: "",
@@ -70,8 +80,9 @@ class App extends Component {
               Search:
               <input
                 type="text"
+                id="query"
                 value={this.state.query}
-                onChange={this.handlequery}
+                onChange={this.handleChange}
               />
               <button>Search</button>
             </form>
@@ -80,22 +91,50 @@ class App extends Component {
           <div className="filterSearch">
             <div className="filterPrintType">
               <p>Paid or Free:</p>
-              <select value={this.state.bookType} onChange={this.handleChange}>
+              <select
+                id="filterType"
+                value={this.state.salesType}
+                onChange={this.handleChange}>
                 <option name="ebooks">All</option>
-                <option name="free-ebooks"> Free-e-book</option>
-                <option name="paid-ebooks">Paid-e-Book</option>
+                <option value="free-ebooks"> Free-e-book</option>
+                <option value="paid-ebooks">Paid-e-Book</option>
+                <option value="partial">Partial</option>
+                <option value="full"> Full</option>
               </select>
             </div>
             <div className="filterBookType">
-              <p>Preview Type:</p>
-              <select value={this.state.bookType} onChange={this.handleChange}>
-                <option name="partial">Partial</option>
-                <option name="full"> Full</option>
+              <p>Print Type:</p>
+              <select
+                id="printType"
+                value={this.state.printType}
+                onChange={this.handleChange}>
+                <option value="all">All</option>
+                <option value="books"> Books</option>
+                <option value="magazines"> Magazines</option>
               </select>
             </div>
-          </div>
 
-          <div className="bookOutputComponent">
+
+          </div>
+          {
+            this.state.books.map(book => (<div className="bookOutputComponent">
+              <div className="bookTitle">
+                <h2>{book.volumeInfo.title}</h2>
+              </div>
+              <div className="bookInfoWrapper">
+                <div className="bookImg">
+                  <img src={book.volumeInfo.imageLinks.smallThumbnail} alt="{book.volumeInfo.title}"></img>
+                </div>
+                <div className="bookInfo">
+                  <p> Author : {book.volumeInfo.authors} </p>
+                  <p> Price: {book.saleInfo.listPrice?.amount} </p>
+                  <p> Description: {book.volumeInfo.description}</p>
+                  <a href={book.volumeInfo.canonicalVolumeLink} target="_new">link text</a>
+                </div>
+              </div>
+            </div>))
+          }
+          {/* <div className="bookOutputComponent">
             <div className="bookTitle">
               <h2>Henry 1</h2>
             </div>
@@ -109,7 +148,7 @@ class App extends Component {
                 <p> Description </p>
               </div>
             </div>
-          </div>
+          </div> */}
 
         </main>
       </div>
